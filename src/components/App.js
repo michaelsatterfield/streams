118f2b062abe68e1db2b4d867df17b015081eea1
Michael Satterfield <michael.f.satterfield@gmail.com>
2021-06-28 16:55:24 -0500

    adding react router dom for navigation

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,25 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+
+const PageOne = () => {
+  return <div>One</div>
+}
+const PageTwo = () => {
+  return <div>Two</div>
+}
+
 
 const App = () => {
   return(
-        <div>App</div>
+        <div>
+
+          <BrowserRouter>
+            <Route path="/" exact component={PageOne} />
+              <Route path="/2" exact component={PageTwo} />
+          </BrowserRouter>
+
+        </div>
   )
 }
 
