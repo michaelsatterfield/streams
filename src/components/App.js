5934c6997c7af34390efef154b266a7e7d0f0d71
Michael Satterfield <michael.f.satterfield@gmail.com>
2021-06-28 20:14:19 -0500

    2

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,30 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import {BrowserRouter, Route} from 'react-router-dom';
 
 
 const PageOne = () => {
-  return <div>One</div>
+    return <div>One
+        <a href={'/2'}>2</a>
+    </div>
 }
 const PageTwo = () => {
-  return <div>Two</div>
+    return <div>Two
+        <a href={'/'}>Home</a>
+    </div>
 }
 
 
 const App = () => {
-  return(
+    return (
         <div>
 
-          <BrowserRouter>
-            <Route path="/" exact component={PageOne} />
-              <Route path="/2" exact component={PageTwo} />
-          </BrowserRouter>
+            <BrowserRouter>
+                <Route path="/" exact component={PageOne}/>
+                <Route path="/2" exact component={PageTwo}/>
+            </BrowserRouter>
 
         </div>
-  )
+    )
 }
 
 export default App
