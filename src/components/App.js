3dd3a724665a14164314da5397d744977a3db3e4
Michael Satterfield <michael.f.satterfield@gmail.com>
2021-07-06 16:12:35 -0500

    making header

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,18 @@
 import React from 'react';
-import {BrowserRouter, Route, Link} from 'react-router-dom';
+import {BrowserRouter, Route, Link, NavLink} from 'react-router-dom';
 import StreamCreate from "./streams/StreamCreate";
 import StreamDelete from "./streams/StreamDelete";
 import StreamEdit from "./streams/StreamEdit";
 import StreamList from "./streams/StreamList";
 import StreamShow from "./streams/StreamShow";
-
+import Header from "./Header";
 
 
 const App = () => {
     return (
         /*use "exact" keyword with Router*/
         <div>
+           <Header></Header>
             <BrowserRouter>
               <div>
                   <Route path={"/"} exact component={StreamList}/>
