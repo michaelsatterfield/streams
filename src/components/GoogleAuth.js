a9ca6e288ea3e3f5660ae1cbba65a436e8eb756b
Michael Satterfield <michael.f.satterfield@gmail.com>
2021-07-11 15:52:02 -0500

    auth

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -1,6 +1,19 @@
 import React, {Component} from 'react';
 
 class GoogleAuth extends Component {
+    componentDidMount() {
+        window.gapi.load('client:auth2', () => {
+            window.gapi.client.init({
+                clientId: '229529128509-dksbjkqub59f2aboevmldfpg4s3eum1q.apps.googleusercontent.com',
+                scope: 'email'
+            }).then(() => {
+                this.auth = window.gapi.auth2.getAuthInstance();
+                // this.setState({isSignedIn: })
+            })
+            });
+    }
+
+
     render() {
         return (
             <div>
