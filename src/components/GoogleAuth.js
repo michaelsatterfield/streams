b797e575d9c94efb82b48fc7b75d85d42840ed8b
Michael Satterfield <michael.f.satterfield@gmail.com>
2021-07-11 15:55:10 -0500

    "auth3"

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -1,6 +1,8 @@
 import React, {Component} from 'react';
 
 class GoogleAuth extends Component {
+    state = { isSignedIn: null };
+
     componentDidMount() {
         window.gapi.load('client:auth2', () => {
             window.gapi.client.init({
