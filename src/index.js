acb1904b0fc89e7e1b8a6e997cfe25699841e6a2
Michael Satterfield <michael.f.satterfield@gmail.com>
2021-07-18 18:40:14 -0500

    redux architecture design

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-
+import {Provider} from 'react-redux';
+import {createStore} from 'redux'
 import App from './components/App';
 
-ReactDom.render(
-    <App/>, document.querySelector('#root')
 
+const store = createStore(reducers);
+
+ReactDom.render(
+    <Provider store={store}>
+        <App/>
+    </Provider>,
+    document.querySelector('#root')
 );
